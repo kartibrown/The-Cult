f8594d071112e3f027248caa402c2edc5f3c0bc0
Adam <69228473+kartibrown@users.noreply.github.com>
2021-07-23 04:38:34 +0200

    Added health to the gui

--- src/creatures/Player.java
+++ src/creatures/Player.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
 
 import controls.Mouse;
 
@@ -15,11 +16,12 @@ public class Player extends Human
 	{
 		name = "Player";
 		color = Color.CYAN;
+		health = 100;
 
 		speed = 2;
 		speedOffset = speed / 2;
 		weight = 0;
-		w = h = 100;
+		w = h = 40;
 		x = contentPaneSize.width / 2 - w / 2;
 		y = contentPaneSize.height / 2 - h / 2;
 
@@ -35,11 +37,17 @@ public class Player extends Human
 		double r = Math.atan2(centerScreen.y - mLoc.y, centerScreen.x - mLoc.x)
 				- Math.PI / 2;
 
+		final AffineTransform saveAT = g.getTransform();
+
 		g.rotate(r, x + w / 2, y + h / 2);
 		g.setColor(color);
 		g.fillOval((int) x, (int) y, (int) w, (int) h);
 		g.setColor(Color.BLACK);
 		g.drawLine((int) (x + w / 2), (int) y, (int) (x + w / 2), (int) y + 10);
+
+		g.setTransform(saveAT);
+		g.setFont(g.getFont().deriveFont(18f));
+		g.drawString(Integer.toString(health), (int) x, (int) y - 20);
 	}
 
 	@Override
