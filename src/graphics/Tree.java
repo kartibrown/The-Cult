b5f3f68438826490ea3e6760f224cbde964dec25
Adam <69228473+kartibrown@users.noreply.github.com>
2021-07-28 01:04:33 +0200

    trying render image

--- src/graphics/Tree.java
+++ src/graphics/Tree.java
@@ -2,14 +2,37 @@ package graphics;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Tree extends Plant
 {
+	BufferedImage leaf1;
+
 	public Tree(final int x, final int y, final int r)
 	{
 		pos.setLocation(x, y);
 		size.setSize(r, r);
 		color = new Color(20, 255, 20);
+
+		try
+		{
+			leaf1 = ImageIO
+					.read(
+							new File(
+									System.getProperty("user.dir")
+											+ "\\data\\leaf.png"
+							)
+					);
+		}
+		catch (final IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -20,6 +43,8 @@ public class Tree extends Plant
 				(int) pos.getX(), (int) pos.getY(), (int) size.getWidth(),
 				(int) size.getHeight()
 		);
+
+		g.drawImage(leaf1, (int) pos.getX(), (int) pos.getY(), null);
 	}
 
 	@Override
