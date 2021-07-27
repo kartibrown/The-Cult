b4f700ce40d7c3d9c575cd4d65503cb8c672c7c6
Adam <69228473+kartibrown@users.noreply.github.com>
2021-07-27 03:05:10 +0200

    added world with 5 trees objects

--- src/graphics/World.java
+++ src/graphics/World.java
@@ -1,6 +1,9 @@
 package graphics;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -8,16 +11,36 @@ public class World extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 
-	public World()
+	final Tree trees[];
+
+	public World(final Dimension contentPaneSize)
 	{
-		this.setBackground(null);
 		this.setOpaque(false);
 		this.setLayout(null);
+
+		trees = new Tree[5];
+
+		final Random rand = new Random();
+
+		for (int i = 0; i < 5; i++)
+		{
+			final int x = rand.nextInt(contentPaneSize.width);
+			final int y = rand.nextInt(contentPaneSize.height);
+			final int r = rand.nextInt(20);
+			trees[i] = new Tree(x, y, r);
+		}
 	}
 
 	@Override
 	public void paintComponent(final Graphics g)
 	{
-		
+		super.paintComponent(g);
+
+		final Graphics2D g2 = (Graphics2D) g;
+
+		for (final Tree tree : trees)
+		{
+			tree.render(g2);
+		}
 	}
 }
