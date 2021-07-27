3b1c28c1e3d5b5ee9f0d528a86fa542920d4f2bb
Adam <69228473+kartibrown@users.noreply.github.com>
2021-07-27 03:07:29 +0200

    Update Entity.java

--- src/graphics/Entity.java
+++ src/graphics/Entity.java
@@ -2,10 +2,11 @@ package graphics;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 
-public class Entity
+public abstract class Entity
 {
 	protected String name;
 	protected Color color;
@@ -21,9 +22,31 @@ public class Entity
 		size = new Dimension();
 	}
 
+	public abstract void render(final Graphics2D g);
+
+	public abstract void physics();
+
 	/*
 	 * GETTERS & SETTERS
 	 */
+
+	public final Point2D getPos()
+	{ return pos; }
+
+	public final Point2D getCenterPos()
+	{ return centerPos; }
+
+	public final double getX()
+	{ return pos.getX(); }
+
+	public final double getY()
+	{ return pos.getY(); }
+
+	public final double getCenterX()
+	{ return centerPos.getX(); }
+
+	public final double getCenterY()
+	{ return centerPos.getY(); }
 
 	public final Color getColor()
 	{ return color; }
