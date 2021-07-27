125d59291bea9d11c0bbc321eaa2ab140fd0bc10
Adam <69228473+kartibrown@users.noreply.github.com>
2021-07-27 03:06:03 +0200

    added world and eased code

--- src/graphics/window/PlayPanel.java
+++ src/graphics/window/PlayPanel.java
@@ -1,6 +1,7 @@
 package graphics.window;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
@@ -10,30 +11,38 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import graphics.World;
 import graphics.creature.Player;
 
 public class PlayPanel extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 
-	private Player player;
+	private final Player player;
+
+	private final World world;
+	private final Dimension contentPaneSize;
 
 	private final Timer timer;
 
-	public PlayPanel(final Player player)
+	public PlayPanel(
+			final Player player, final World world,
+			final Dimension contentPaneSize
+	)
 	{
 		this.setBackground(Color.DARK_GRAY);
+		this.setOpaque(true);
 		this.setLayout(new GridBagLayout());
 
 		this.player = player;
+		this.world = world;
+		this.contentPaneSize = contentPaneSize;
 
 		timer = new Timer(30, new ActionListener()
 		{
 			@Override
 			public void actionPerformed(final ActionEvent e)
-			{
-				repaint();
-			}
+			{ repaint(); }
 		});
 	}
 
@@ -49,7 +58,14 @@ public class PlayPanel extends JPanel
 		t1.start();
 
 		super.paintComponent(g);
+
+		final int cameraX = (int) (contentPaneSize.width / 2 - player.getX());
+		final int cameraY = (int) (contentPaneSize.height / 2 - player.getY());
+		g.translate(cameraX, cameraY);
+
+		world.paintComponent(g);
 		final Graphics2D g2 = (Graphics2D) g;
+		player.setCameraLocation(cameraX, cameraY);
 		player.render(g2);
 		player.physics();
 		player.move();
